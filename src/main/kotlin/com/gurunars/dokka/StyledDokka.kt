32bcd3cead2fc86ea20596ef894a3962d020280e
Anton Berezin <gurunars@gmail.com>
2017-07-15 18:03:08 +0300

    Fixed dokka reference

--- src/main/kotlin/com/gurunars/dokka/StyledDokka.kt
+++ src/main/kotlin/com/gurunars/dokka/StyledDokka.kt
@@ -3,9 +3,14 @@ package com.gurunars.dokka
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaBasePlugin
-import org.jetbrains.dokka.ExternalDocumentationLinkImpl
-import org.jetbrains.dokka.gradle.DokkaTask
-import java.net.URL
+import org.jetbrains.dokka.gradle.DokkaAndroidTask
+
+
+private fun requirePlugins(vararg pluginNames: String) : (project: Project) -> Boolean {
+    return { project ->
+        pluginNames.all { project.plugins.findPlugin(it) != null }
+    }
+}
 
 
 class StyledDokka : Plugin<Project> {
@@ -16,28 +21,22 @@ class StyledDokka : Plugin<Project> {
                 description = "Aggregate API docs of all subprojects with custom styles."
                 group = JavaBasePlugin.DOCUMENTATION_GROUP
 
-                val modules = project.subprojects.filter {
-                    it.plugins.findPlugin("com.android.library") != null
-                }
+                val modules = project.subprojects.filter(requirePlugins(
+                    "com.android.library", "kotlin-android"
+                ))
 
                 modules.forEach {
-                    it.tasks.replace("dokka", DokkaTask::class.java).apply {
+                    it.tasks.replace("dokka", DokkaAndroidTask::class.java).apply {
                         moduleName=it.name
                         sourceDirs = it.files("src/main/kotlin")
                         outputFormat = "html"
                         outputDirectory = "html-docs"
                         includes = listOf("README.md")
-                        externalDocumentationLinks = mutableListOf(
-                                ExternalDocumentationLinkImpl(
-                                        url= URL("https://developer.android.com/reference/"),
-                                        packageListUrl= URL("https://developer.android.com/reference/package-list")
-                                )
-                        )
                     }
                 }
 
                 setMustRunAfter(
-                        modules.map { it.getTasksByName("dokka", true) }.flatten()
+                    modules.map { it.getTasksByName("dokka", true) }.flatten()
                 )
 
                 doLast { beautify(project, modules) }
